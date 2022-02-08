7813423f0dcb0cd92dafe39b51f42c2050ab0cc1
AlanJhonatan <afranca2@lenovo.com>
2022-02-08 10:11:15 -0300

    chore: added routing system using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,3 @@
-import { useState } from 'react';
-import { Tweet } from './components/Tweet';
+import { AppRoutes } from './routes';
 
-export function App() {
-  const [tweets, setTweets] = useState<string[]>([
-    'Tweet 1',
-    'Tweet 2',
-    'Tweet 3',
-  ]);
-
-  function createTweet() {
-    setTweets([...tweets, 'Tweet ' + (tweets.length + 1)]);
-  }
-
-  return (
-    <>
-      {tweets.map((tweet) => {
-        return <Tweet text={tweet} />;
-      })}
-
-      <button onClick={createTweet}>Add Tweet</button>
-    </>
-  );
-}
+export const App = () => <AppRoutes />;
